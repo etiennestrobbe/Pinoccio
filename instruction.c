ecd538846e0c1c715f209292a86b39de0c1072aa
DUPANLOUP <remy@Dupanloup-Vostro-2520.(none)>
2014-06-13 10:53:06 +0200

    Doxygene pour instruction.c

--- instruction.c
+++ instruction.c
@@ -1,19 +1,45 @@
+/**
+ * \file instruction.c
+ * \brief Affichage des instructions
+ * \author Rémy.D
+ * \date 10 juin 2014
+ *
+ * Programme permettant l'affichage d'une instruction sous la forme : COP REG, OP
+ *
+ */
 #include "instruction.h"
 #include <stdio.h>
 
+//! Forme imprimable des codes opérations
 const char* cop_names[] = {"ILLOP", "NOP", "LOAD", "STORE", "ADD", "SUB", "BRANCH", "CALL", "RET", "PUSH", "POP", "HALT"};
 
+//! Forme imprimable des conditions
 const char* condition_names[] = {"NC", "EQ", "NE", "GT", "GE", "LT", "LE"};
 
+/**
+ * \fn void print_registre(Instruction i)
+ * \brief Affiche le registre de l'instruction
+ * \param i, l'instruction a afficher.
+ */
 void print_registre(Instruction i){
 	(i.instr_generic._regcond >= 10) ? printf("R%d, ", i.instr_generic._regcond) : printf("R0%d, ", i.instr_generic._regcond);
 	// Ca dépote le ternaire !
 }
 
+/**
+ * \fn void print_condition(Instruction i)
+ * \brief Affiche la condition de l'instruction
+ * \param i, l'instruction a afficher.
+ */
 void print_condition(Instruction i){
-	printf("%s, ",condition_names[i.instr_generic._regcond]); //VERIFIER QUE i.instr_generic._regcond <= 6 !!!!!!!!! (lors de l'appelle)
+	printf("%s, ",condition_names[i.instr_generic._regcond]);
 }
 
+/**
+ * \fn void print_operande(Instruction i)
+ * \brief Affiche l'opérande de l'instruction: la valeur immédiate ou l'adresse absolue ou l'adresse indexée
+ * \param i, l'instruction a afficher.
+ */
 void print_operande(Instruction i){
 	if (i.instr_generic._immediate == 1){
 		printf("#%d", i.instr_immediate._value);
@@ -21,9 +47,8 @@ void print_operande(Instruction i){
 	else{
 		if (i.instr_generic._indexed == 1){
 			(i.instr_indexed._rindex >= 10) ? printf("%d[R%d]", i.instr_indexed._offset, i.instr_indexed._rindex) : printf("%d[R0%d]", i.instr_indexed._offset, i.instr_indexed._rindex);
-			//Encore un !
 		}
-		else{ //TESTER LES 4 CAS !
+		else{
 			if (i.instr_absolute._address < 16){
 				printf("@0x000%x", i.instr_absolute._address); //%x permet l'affiche en hexadecimal
 			}
@@ -40,8 +65,12 @@ void print_operande(Instruction i){
 	}
 }
 
+/**
+ * \fn void print_instruction(Instruction instr, unsigned addr)
+ * \brief Affiche une instruction en entière
+ * \param i, l'instruction a afficher.
+ */
 void print_instruction(Instruction instr, unsigned addr){
-	//printf("0x%u: ", addr); NE SERT A RIEN
 	printf("%s ",cop_names[instr.instr_generic._cop]);
 	switch(instr.instr_generic._cop){
 		case LOAD:
